1efcbddd3eed0f6aab10f00699bb55836680dc36
Ivor Wanders <ivor@iwanders.net>
2024-01-28 15:35:37 -0500

    List all requests in comment.

--- irpmon_thcbase.py
+++ irpmon_thcbase.py
@@ -707,11 +707,13 @@ REQUEST with 120 data, first byte: 0x06
 None
 
 ^ This could be frequencies.
-1212713792
+1127350272
 1127612416
 1127481344
 1065353216
 1127612416
+...
+1073741824
 
 
 Irp(index=25, irp_id=172, function=<Function.PnP: 3>, time='2024-01-28 6:29:45 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660298777472, data=[], previous_mode=<Mode.KernelMode: 1>, requestor_mode=<Mode.KernelMode: 1>)
@@ -720,6 +722,14 @@ response with 292 data, first byte: 0x50
 PCI\VEN_8086&DEV_51D0&SUBSYS_00641414&REV_01
 
 Heh.
+
+58-59;
+08 D1 2E 9A 00 00 00 00 00 00 93 00 00 00 00 FF 00 00 08 08 00 08 00 90 02 03 4A 76 00 03 08 08 00 2E 44 00 2D 00 43 07 00 04 08 44 00 1
+                                                                    |sq|timestamp  |
+
+
+Irp(index=59, irp_id=2797, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:53 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660298778288, data=[], previous_mode=<Mode.KernelMode: 1>, requestor_mode=<Mode.KernelMode: 1>)
+dimenions, frequency noise, type 7, type 50, type 18, 
 """
 
 def run_print_requests(args):
@@ -735,6 +745,90 @@ def run_print_requests(args):
         if r.data[0] == 0x65 and len(r.data) == 7488:
             continue
         print(hexdump(r.data))
+
+"""
+Requests aren't actually that shocking;
+Irp(index=47, irp_id=1089, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:52 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660351456944, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 61 data, first byte: 0x60
+60 01 00 00 04 89 11 00 22 40 11 5d 00 84 85 03 20 1c 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ef ef ef ef ee 0e 00 20 14 17 01 80 14 17 01 80 00 00 00 00 00 00 00 00 6c 2b 00 20 
+None
+
+Irp(index=55, irp_id=2751, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:53 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660409934656, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 120 data, first byte: 0x06
+06 77 00 00 00 00 00 00 70 00 00 00 00 02 01 2e 00 00 00 44 00 00 00 fd 6a 00 00 53 47 00 00 01 41 65 cc 43 00 00 00 00 00 00 00 00 00 00 00 00 b6 e0 ca 43 00 00 00 00 00 00 32 43 00 00 36 43 
+00 00 34 43 00 00 80 3f 00 00 32 43 00 00 36 43 00 00 34 43 00 00 80 3f 00 00 b4 42 00 00 2b 43 00 00 c8 42 00 00 a0 41 00 00 2c 43 00 00 31 43 00 00 2f 43 00 00 00 40 
+None
+
+This is that one that could be frequencies.
+
+Irp(index=56, irp_id=2767, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:53 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660402932944, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x09
+09 8e a5 00 02 00 00 00 00 00 00 00 00 00 00 00 
+None
+Shares the '09 8e a5' with the later status type requests.
+
+Irp(index=57, irp_id=2779, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:53 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660410081296, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x05
+05 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+None
+
+Irp(index=69, irp_id=9154, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:58 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660530637152, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 2 data, first byte: 0x70
+70 02 
+None
+
+Irp(index=70, irp_id=9155, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:58 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660273998544, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x70
+70 01 00 00 00 00 00 00 37 00 35 00 32 00 45 00 
+None
+
+Irp(index=71, irp_id=9157, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:29:58 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660533136416, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x56
+56 b4 88 12 64 7a 68 00 00 00 00 00 00 00 00 00 
+None
+
+Irp(index=75, irp_id=11987, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:30:01 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660539996448, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x09
+09 8e a5 01 02 00 00 00 00 00 00 00 00 70 17 00 
+
+...
+
+Irp(index=146, irp_id=12277, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:30:02 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660541876512, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x09
+09 8e a5 15 02 00 00 00 00 ad f7 d8 97 70 17 00 
+None
+
+...
+Irp(index=2706, irp_id=19229, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:30:07 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446694660544420832, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x09
+09 8e a5 f0 02 01 00 90 01 00 00 00 00 52 15 00 
+None
+...
+
+Irp(index=6335, irp_id=15648, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:32:08 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446604998442064160, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x09
+09 8e a5 8c 02 00 00 00 00 ad f7 d8 97 70 17 00 
+None
+..
+Irp(index=7879, irp_id=18838, function=<Function.InternalDeviceControl: 9>, time='2024-01-28 6:32:12 PM', status=<Status.STATUS_SUCCESS: 1>, address=18446604998452066976, data=[], previous_mode=<Mode.UserMode: 2>, requestor_mode=<Mode.UserMode: 2>)
+REQUEST with 16 data, first byte: 0x09
+09 8e a5 45 02 01 00 90 01 00 00 00 00 79 15 00 
+None
+
+... 
+09 8e a5 45 02 01 00 90 01 00 00 00 00 79 15 00 
+        |sq|
+
+sq is sequency number.
+Message is resent every two or three receipts.
+
+It's this; when pen is used;
+09 8e a5 15 02 00 00 00 00 ad f7 d8 97 70 17 00 
+
+It's this; when touch is used:
+09 8e a5 cd 02 01 00 90 01 00 00 00 00 e5 14 00
+
+"""
 
 def run_things(args):
     records = load_file(args.in_file)
