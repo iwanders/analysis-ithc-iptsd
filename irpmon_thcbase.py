c7868d2848023ef07d5583fe0bde779aaa6f66bf
Ivor Wanders <ivor@iwanders.net>
2024-01-28 08:56:30 -0500

    Comments on how the dump looks.

--- irpmon_thcbase.py
+++ irpmon_thcbase.py
@@ -232,94 +232,6 @@ Aha. so the first three bytes are 'bad'.
 
 Yes!
 
-For concat mid;
-
-
-
-#define IPTS_DFT_NUM_COMPONENTS 9
-#define i16 s16
-#define i8 s8
-
-struct ipts_hid_frame {
-	u32 size;
-	u8 reserved1;
-	u8 type;
-	u8 reserved2;
-};
-
-
-struct  ipts_report {
-	u8 type;
-	u8 flags;
-	u16 size;
-};
-
-
-u8 IPTS_DFT_ID_POSITION = 6;
-u8 IPTS_DFT_ID_POSITION2 = 7;
-u8 IPTS_DFT_ID_BUTTON   = 9;
-u8 IPTS_DFT_ID_PRESSURE = 11;
-
-struct ipts_pen_dft_window_row {
-	u32 frequency;
-	u32 magnitude;
-	i16 real[IPTS_DFT_NUM_COMPONENTS]; // NOLINT(modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
-	i16 imag[IPTS_DFT_NUM_COMPONENTS]; // NOLINT(modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
-	i8 first;
-	i8 last;
-	i8 mid;
-	i8 zero;
-};
-
-struct ipts_pen_dft_window {
-	u32 timestamp; // counting at approx 8MHz
-	u8 num_rows;
-	u8 seq_num;
-	u8 reserved[3]; // NOLINT(modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
-	u8 data_type;
-	u8 reserved2[2]; // NOLINT(modernize-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays)
-	ipts_pen_dft_window_row x[num_rows];
-		ipts_pen_dft_window_row y[num_rows];
-};
-
-
-struct combined {
-    ipts_report header;
-    
-      match (header.type, header.size) {
-        (0x5c, _): ipts_pen_dft_window window;
-        (_, _): u8 data[header.size];
-      }
-    
-};
-
-combined first[7] @ 0x1d;
-combined second[7] @ 0xa1d;
-combined third[7] @ 0x2049;
-
-combined perhaps_something[1] @ 0x1e4d;
-
-
-struct overarching {
- u32 outer_size;
- u8 pad[3];
- u32 inner_size;
- u8 pad2[7];
- //u8 data[inner_size - 4];
- combined d[9];
-};
-
-//overarching base @ 0x03;
-overarching base @ 0x3a83;
-
-
-
-
-
-
-
-
-
 
 """
 
@@ -584,6 +496,70 @@ timestamp: 460965579
 Ah yes, and this matches data from chunk 1390
 
 Does show that the second blob is junk, it's duplicated between chunk 1390 and 1392.
+
+"""
+
+
+"""
+for dump;
+
+
+#define f32 float
+
+struct  DeviceInfo {
+	u16 vendor;
+	u16 product;
+    u8 pad[4];
+	u64 buffer_size;
+};
+
+struct ipts_touch_metadata_size {
+	u32 rows;
+	u32 columns;
+	u32 width;
+	u32 height;
+};
+
+struct  ipts_touch_metadata_transform {
+	f32 xx, yx, tx;
+	f32 xy, yy, ty;
+};
+
+struct ipts_touch_metadata_unknown {
+	f32 unknown[16];
+};
+
+struct ipts_full_metadata {
+    ipts_touch_metadata_size size;
+    u8 unknown;
+    ipts_touch_metadata_transform tf;
+    ipts_touch_metadata_unknown unknown2;
+};
+
+struct Block<auto Size> {
+  u64 real_size;
+  //u8 data[Size];
+};
+
+
+struct IptsDump {
+    DeviceInfo info;
+    u8 has_metadata;
+    if (has_metadata != 0) {
+      ipts_full_metadata metadata;
+    }
+    Block<info.buffer_size> blocks[1];
+};
+
+IptsDump dump @ 0x0;
+
+struct iptsblock {
+ u64 size;
+ u8 data[size];
+};
+
+//iptsblock a[1] @ 0x94;
+
 
 """
 
