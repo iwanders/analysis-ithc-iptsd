1a51895dba52778f9d71e4605dd50769ccaf3c28
Ivor Wanders <ivor@iwanders.net>
2024-02-19 10:23:31 -0500

    Printing button rows.

--- button.py
+++ button.py
@@ -4,9 +4,20 @@ import sys
 import json
 
 from ipts import iptsd_read, extract_reports, IptsDftWindowPosition, IptsDftWindowButton, IptsDftWindowPressure, group_reports
-from iptsd import IptsdConfig, clamp, obtain_state, write_states
+from iptsd import IptsdConfig, clamp, obtain_state, write_states, _convert_row
 # from generalised import generalise_digi, wintilt_to_yaw_tilt
 from digi_info import load_digiinfo_xml
+
+def print_dft(d):
+    def format_r(r):
+        combs = [f"{r.real[i]: >4}, {r.imag[i]: >4}" for i in range(9)]
+        v = " ".join(list(f"{comb: >20s}" for comb in combs))
+        return v
+
+    for i, r in enumerate(d.x):
+        print(f"x[{i}]: {r.magnitude: >8d} {format_r(r)}")
+    for i, r in enumerate(d.y):
+        print(f"y[{i}]: {r.magnitude: >8d} {format_r(r)}")
 
 if __name__ == "__main__":
     z = iptsd_read(sys.argv[1])
@@ -21,6 +32,8 @@ if __name__ == "__main__":
 
     for s in states:
         # del s["group"]
-        print(s.get("button", "-"))
-        # if s.get("button", False):
-            # print(s["group"][IptsDftWindowButton])
+        # print(s.get("button", "-"))
+        if s.get("button", False):
+            button = s["group"][IptsDftWindowButton]
+            print_dft(button)
+            print()
